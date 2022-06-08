361edfdb1a200bb02a62781bce3371113ce9486c
Michael Harris <mharris@luabase.com>
2022-06-08 16:39:03 +0000

    Allow for dev databases

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -3,11 +3,27 @@
     {%- set default_schema = target.schema -%}
     {%- if custom_schema_name is none -%}
 
-        {{ default_schema }}
+        {% if target.name == "dev" %}
+
+            {{ target.schema }}
+
+        {%- else -%}
+        
+            {{ default_schema }}
+
+        {% endif %}
 
     {%- else -%}
 
-        {{ custom_schema_name | trim }}
+        {% if target.name == "dev" %}
+
+            {{ target.schema }}_{{ custom_schema_name | trim }}
+
+        {%- else -%}
+        
+            {{ custom_schema_name | trim }}
+
+        {% endif %}
 
     {%- endif -%}
 
