abc208d8fa52ae1562cfe7d5a40375a9b3edccd7
Michael Harris <mharris@luabase.com>
2022-07-07 15:37:03 -0400

    Use project var

--- analyses/ethereum_maple_pool_events.sql
+++ analyses/ethereum_maple_pool_events.sql
@@ -1,9 +1,10 @@
+{# https://maplefinance.gitbook.io/maple/smart-contracts/pools/pool#events #}
 select *
 from {{ ref('events') }}
 where address in (
     select distinct lower(value)
     from {{ ref('events') }}
-    where address = '0x2cd79f7f8b38b9c0d80ea6b230441841a31537ec'
+    where address = '{{ var('maple_pool_factory_address') }}'
         and abi_name = 'PoolCreated'
         and name = 'pool'
 )
