13b69a307fa3341be331e184be705fdf1cb5b08a
Michael Harris <mharris@luabase.com>
2022-07-09 08:28:40 -0400

    Update ref

--- analyses/ethereum_maple_pool_events.sql
+++ analyses/ethereum_maple_pool_events.sql
@@ -1,9 +1,9 @@
 {# https://maplefinance.gitbook.io/maple/smart-contracts/pools/pool#events #}
 select *
-from {{ ref('events') }}
+from {{ ref('ethereum_events') }}
 where address in (
     select distinct lower(value)
-    from {{ ref('events') }}
+    from {{ ref('ethereum_events') }}
     where address = '{{ var('maple_pool_factory_address') }}'
         and abi_name = 'PoolCreated'
         and name = 'pool'
