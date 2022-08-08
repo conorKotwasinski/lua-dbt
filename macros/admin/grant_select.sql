9317df917a33f9c62a2211683c68171a3a91c82d
Michael Harris <mharris@luabase.com>
2022-08-08 15:45:24 -0400

    Add `lua_api` user to read-only grants (#70)

--- macros/admin/grant_select.sql
+++ macros/admin/grant_select.sql
@@ -1,7 +1,7 @@
 {% macro grant_select(table) %}
 {% if target.name == 'prod' %}
     {% set sql %}
-        grant on cluster 'lua-2' select, show tables on {{ table }} to demo_user, teams_admin
+        grant on cluster 'lua-2' select, show tables on {{ table }} to demo_user, teams_admin, lua_api
     {% endset %}
     {% do run_query(sql) %}
 {% else %}
