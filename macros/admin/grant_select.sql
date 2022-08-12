709f4cd4853ac249c0a1fef530ef8f82c72fb51b
Michael Harris <mharris@luabase.com>
2022-08-12 13:23:48 -0400

    Add debug log

--- macros/admin/grant_select.sql
+++ macros/admin/grant_select.sql
@@ -1,5 +1,6 @@
 {% macro grant_select(table) %}
 {% if target.name == 'prod' %}
+    {{ log("Granting SELECT on " ~ table) }}
     {% set sql %}
         grant on cluster 'lua-2' select, show tables on {{ table }} to demo_user, teams_admin
     {% endset %}
