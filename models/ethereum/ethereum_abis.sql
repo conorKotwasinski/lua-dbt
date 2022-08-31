4222322e707e1bf1ea6856c6dc3ae69aca705b44
Michael Harris <mharris@luabase.com>
2022-08-31 09:00:46 -0400

    Remove deprecated field

--- models/ethereum/ethereum_abis.sql
+++ models/ethereum/ethereum_abis.sql
@@ -2,8 +2,7 @@ select
     address,
     name,
     abi,
-    base64Decode(source_code) as source_code,
-    deployer_address
+    base64Decode(source_code) as source_code
 from {{ source('contract', 'details') }}
 where public
     and source = 'https://api.etherscan.io'
